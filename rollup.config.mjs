fa38df54a7247049a18177e48f4c53a33850defd
Saylor Allf <online@sallf.com>
2024-05-06 09:25:39 -0600

    add rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
+import typescript from '@rollup/plugin-typescript'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      typescript(),
+      babel({
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        babelHelpers: 'bundled',
+      }),
+    ],
+  },
+]
